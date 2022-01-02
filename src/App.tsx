f3170daf4b24bfd0b3eb00e7f261de298ae97284
Masahito Osako <velonica.gale2227@gmail.com>
2022-01-02 23:09:48 +0900

    add button validation

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,9 @@ import Simulator from "./components/simulator"
 
 export const App: React.FC = () => {
   const [initValue, setInitValue] = useState('')
+  const [script, setScript] = useState('')
   const handleInitValue = (event: React.ChangeEvent<HTMLInputElement>) => setInitValue(event.target.value)
+  const handleScript = (event: React.ChangeEvent<HTMLTextAreaElement>) => setScript(event.target.value)
 
   return (
     <ChakraProvider theme={theme}>
@@ -26,13 +28,24 @@ export const App: React.FC = () => {
         <VStack pt='100px' pb={10} px={{ base: 4, md: 8 }} w='100%'>
           <Heading fontFamily='heading' as='h1' fontSize='4xl' mb={10}>Turing Machine Simulator</Heading>
           <Simulator initValue={initValue} />
-          <ButtonGroup spacing={{ base: 2, md: 4 }} w='100%' justifyContent='center'>
+          <ButtonGroup spacing={{ base: 2, md: 4 }} w='100%' justifyContent='center' isDisabled={initValue === '' || script === ''}>
             <Button colorScheme='green' p={4} mr='-px' w={{ base: 'sm', md: 'sm' }}>Run</Button>
             <Button w={{ base: 'sm', md: 'md' }} colorScheme='red'>Step</Button>
           </ButtonGroup>
           <Box mt={4} d='block' w='100%' px={{ base: 0, md: '15%' }}>
-            <Input placeholder='initial value' mt={4} borderWidth={2} w='100%' onChange={handleInitValue} />
-            <Textarea resize='vertical' fontFamily='mono' borderWidth={2} w='100%' h='400px' px={2} mt={2} placeholder="Here is script for turing machine" size='lg' />
+            <Input placeholder='initial value' mt={4} borderWidth={2} w='100%' onChange={handleInitValue} fontFamily='mono' />
+            <Textarea
+              resize='vertical'
+              fontFamily='mono'
+              borderWidth={2}
+              w='100%'
+              h='400px'
+              px={2}
+              mt={2}
+              placeholder="Here is script for turing machine"
+              size='lg'
+              onChange={handleScript}
+            />
           </Box>
         </VStack>
       </Main>
