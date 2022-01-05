4d76893fadbf16d2445b6246821ba245ace43dd1
Masahito Osako <velonica.gale2227@gmail.com>
2022-01-05 23:42:26 +0900

    update

--- src/components/script_area.tsx
+++ src/components/script_area.tsx
@@ -1,27 +1,42 @@
-import React, { useState } from 'react'
+import React from 'react'
 import {
   Textarea
 } from '@chakra-ui/react'
 
-const ScriptArea = () => {
-  const [script, setScript] = useState('')
-  const handleScript = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
-    setScript(event.target.value)
+interface ScriptAreaProps {
+  setWrittenScript: (bool: boolean) => void
+}
+interface ScriptAreaState {
+  script: string
+}
+export default class ScriptArea extends React.Component<ScriptAreaProps, ScriptAreaState> {
+  constructor(props: ScriptAreaProps) {
+    super(props)
+    this.state = {
+      script: ''
+    }
+    this.handleScript = this.handleScript.bind(this)
   }
 
-  return (
-    <Textarea
-      resize='vertical'
-      fontFamily='mono'
-      borderWidth={2}
-      w='100%'
-      h='400px'
-      px={2}
-      mt={2}
-      placeholder="Here is script for turing machine"
-      size='lg'
-      onChange={handleScript}
-    />
-  )
+  handleScript = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
+    this.setState({ script: event.target.value })
+    this.props.setWrittenScript(event.target.value !== '')
+  }
+
+  render() {
+    return (
+      <Textarea
+        resize='vertical'
+        fontFamily='mono'
+        borderWidth={2}
+        w='100%'
+        h='400px'
+        px={2}
+        mt={2}
+        placeholder="Here is script for turing machine"
+        size='lg'
+        onChange={this.handleScript}
+      />
+    )
+  }
 }
-export default ScriptArea
